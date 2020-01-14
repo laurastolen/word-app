bbd91b7853ed0b4491acd9728c9cb32123c32e88
laura antonius <laurastolen@gmail.com>
2020-01-14 17:08:44 -0500

    setup owlbot API to return search results for user input

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 'use strict';
 
+// SETUP AND REQUIREMENTS
 const express = require('express');
 let app = express();
-
-require('dotenv').config();
-const PORT = process.env.PORT || 3001;
-
 require('ejs');
 app.set('view engine', 'ejs');
-
+require('dotenv').config();
+const PORT = process.env.PORT || 3001;
+// const superagent = require('superagent');
+const owlbot = require('owlbot-js');
+const owlquery = owlbot(process.env.OWLBOT_TOKEN);
+app.use(express.urlencoded());
 app.listen(PORT, () => {
   console.log(`up on port ${PORT}`);
 });
+
 
 // ROUTES
 app.get('/', showHome);
 app.get('/words', showMyWords);
 app.get('/search', showSearchPage);
-app.get('/results', saveWord)
+app.get('/results', findWord);
 
 app.post('/search', findWord);
 app.post('/results', saveWord);
 
+
+// FUNCTIONS
 function showHome(req, res) {
   res.render('index');
 }
@@ -35,7 +40,18 @@ function showSearchPage(req, res) {
 }
 
 function findWord(req, res) {
-  res.send('here we will search the owl thing for a word based on form input');
+  let word = req.body.word;
+
+  owlquery.define(word)
+    .then(result => {
+      console.log(result);
+
+    })
+    .catch(error => console.log(error)
+    );
+
+  res.render('results', { word: word });
+
 }
 
 function saveWord(req, res) {
